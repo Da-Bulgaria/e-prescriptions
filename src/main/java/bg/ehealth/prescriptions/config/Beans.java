dc3ee97815334bd224ce2d0f22ef4d2dc1fadbf8
Bozho <bozhidar.bozhanov@gmail.com>
2020-03-27 12:16:07 +0200

    google authenticator bean

--- src/main/java/bg/ehealth/prescriptions/config/Beans.java
+++ src/main/java/bg/ehealth/prescriptions/config/Beans.java
@@ -1,10 +1,18 @@
 package bg.ehealth.prescriptions.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
 
 @Configuration
 @EnableConfigurationProperties(DummyProperties.class)
 public class Beans {
+    
+    @Bean
+    public GoogleAuthenticator googleAuthenticator() {
+        return new com.warrenstrange.googleauth.GoogleAuthenticator();
+    }
     
 }
