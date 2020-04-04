c6844f146de9033297b7efd82e1ae8e94f488661
Bozho <bozhidar.bozhanov@gmail.com>
2020-04-04 12:26:59 +0300

    simplified login form

--- src/main/resources/assets/js/login.js
+++ src/main/resources/assets/js/login.js
@@ -1,70 +1,61 @@
 $(document).ready(function() {
-	var loginDoctor = $("#loginDoctor");
-	var userInputDoctor = $("#userInputDoctor");
-	var passwordDoctor = $(""#passwordDoctor");
+	let loginDoctor = $("#loginDoctor");
+	let loginPharmacist = $("#loginPharmacist");
 	
-	var loginPharmacist = $("#loginPharmacist");
-	var userInputPharmacist = $("#userInputPharmacist");
-	var passwordPharmacist = $("#passwordPharmacist");
-	
-	function bodyConstructor(userType) {
-	    let userInput = "";
-	    let userPassword;
-	    let userType;
-	
-	    if (userType == "DOCTOR") {
-	        userPassword = passwordDoctor.val();
-	        userInput = userInputDoctor.val();
-	    } else if (userType == "PHARMACIST") {
-	        userPassword = passwordPharmacist.val();
-	        userInput = userInputPharmacist.val();
-	    }
-	
-	    if (userInput.toString().includes("@")) {
-	        return JSON.stringify({email: userInput, password: userPassword, userType: userType});
-	    } else {
-	        return JSON.stringify({uin: userInput, password: userPassword, userType: userType});
-	    }
-	}
-	
-	async function logUser() {
-	
-	    try {
-	        const response = await fetch("/user/login", {
-	            method: 'POST',
-	            credentials: 'include',
-	            headers: {
-	                'Content-Type': 'application/json',
-	                'Cache-Control': 'no-cache'
-	            },
-	            body: bodyConstructor()
-	        });
-	
-	        if (response.status == 200) {
-	            clearForm();
-	
-	            window.location = "/";
-	        } else {
-	            console.log('Login failed.');
-	            let error = new Error(response.statusText);
-	            error.response = response;
-	            throw error;
-	        }
-	    } catch (error) {
-	        // console.error(
-	        //     'Logging error.',
-	        //     error
-	        // );
-	
-	        // const { response } = error;
-	        // setUserData(
-	        //     Object.assign({}, userData, {
-	        //         error: response ? response.statusText : error.message
-	        //     })
-	        // )
-	    }
-	}
-	
-	loginDoctor.on('click', logUser("DOCTOR"));
-	loginPharmacist.on('click', logUser("PHARMACIST"));
-})
+	loginDoctor.on('click', function() {
+		logUser("DOCTOR");
+	});
+
+	loginPharmacist.on('click', function() {
+		logUser("PHARMACIST");
+	});
+});
+
+function buildBody(userType) {
+    let userInput = "";
+    let userPassword;
+
+    if (userType == "DOCTOR") {
+    	let userInputDoctor = $("#userInputDoctor");
+    	let passwordDoctor = $("#passwordDoctor");
+        userPassword = passwordDoctor.val();
+        userInput = userInputDoctor.val();
+    } else if (userType == "PHARMACIST") {
+    	let userInputPharmacist = $("#userInputPharmacist");
+    	let passwordPharmacist = $("#passwordPharmacist");
+    	
+        userPassword = passwordPharmacist.val();
+        userInput = userInputPharmacist.val();
+    }
+
+    if (userInput.toString().includes("@")) {
+        return JSON.stringify({email: userInput, password: userPassword, userType: userType});
+    } else {
+        return JSON.stringify({uin: userInput, password: userPassword, userType: userType});
+    }
+}
+
+async function logUser(userType) {
+    try {
+        const response = await fetch("/user/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache'
+            },
+            body: buildBody(userType)
+        });
+
+        if (response.status == 200) {
+            window.location = "/";
+        } else {
+            console.log('Login failed.');
+            let error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+        }
+    } catch (error) {
+         console.error('Login error.', error);
+    }
+}
+
