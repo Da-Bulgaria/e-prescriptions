f4c768c7066bd3cee0601acf66abdd2886aacf66
Ekaterina_Mihaylova <katya@maj.io>
2020-04-08 15:27:57 +0300

    Imported the International Classification of Deseases. Created 2 in memory indexes on it - for search and for autocompletion ref icd10

---
+++ src/main/java/bg/ehealth/prescriptions/services/icd/ICDEntity.java
@@ -0,0 +1,22 @@
+package bg.ehealth.prescriptions.services.icd;
+
+public class ICDEntity {
+    private String id;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
